ae1e3aade66f007c2b3551ae468acc0e8b20212b
Rafal Giemza <rafalgiemza@gmail.com>
2022-02-22 21:57:12 +0100

    publish best food app project page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,10 +1,10 @@
 const projectsData = [
-  // {
-  //   title: 'Best Food App',
-  //   description: `Prosta aplikacja do zamawiania jedzenia. Krok po kroku.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/best-food',
-  // },
+  {
+    title: 'Best Food App',
+    description: `Prosta aplikacja do zamawiania jedzenia. Krok po kroku.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/best-food',
+  },
 ]
 
 export default projectsData
