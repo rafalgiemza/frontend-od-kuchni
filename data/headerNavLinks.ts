c2e2cf731727257e5c2252726e15758afe8d107f
Rafal Giemza <rafalgiemza@gmail.com>
2022-03-08 23:57:30 +0100

    publish article 002 of best food app

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tagi', title: 'Tagi' },
-  { href: '/projekty', title: 'Projekty' },
+  // { href: '/projekty', title: 'Projekty' },
   { href: '/kontakt', title: 'Kontakt' },
 ]
 
